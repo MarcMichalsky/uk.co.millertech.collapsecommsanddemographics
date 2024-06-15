198fc204da82b0931e9435f92902e9dbba4a0410
Marc Michalsky <michalsky@forumzfd.de>
2024-06-15 10:58:52 +0200

    civix upgrade

--- collapsecommsanddemographics.php
+++ collapsecommsanddemographics.php
@@ -32,16 +32,6 @@ function collapsecommsanddemographics_civicrm_config(&$config)
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function collapsecommsanddemographics_civicrm_xmlMenu(&$files)
-{
-    _collapsecommsanddemographics_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -49,26 +39,6 @@ function collapsecommsanddemographics_civicrm_xmlMenu(&$files)
 function collapsecommsanddemographics_civicrm_install()
 {
     _collapsecommsanddemographics_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function collapsecommsanddemographics_civicrm_postInstall()
-{
-    _collapsecommsanddemographics_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function collapsecommsanddemographics_civicrm_uninstall()
-{
-    _collapsecommsanddemographics_civix_civicrm_uninstall();
 }
 
 /**
@@ -80,99 +50,6 @@ function collapsecommsanddemographics_civicrm_enable()
 {
     _collapsecommsanddemographics_civix_civicrm_enable();
 }
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function collapsecommsanddemographics_civicrm_disable()
-{
-    _collapsecommsanddemographics_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function collapsecommsanddemographics_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL)
-{
-    return _collapsecommsanddemographics_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function collapsecommsanddemographics_civicrm_managed(&$entities)
-{
-    _collapsecommsanddemographics_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function collapsecommsanddemographics_civicrm_caseTypes(&$caseTypes)
-{
-    _collapsecommsanddemographics_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function collapsecommsanddemographics_civicrm_angularModules(&$angularModules)
-{
-    _collapsecommsanddemographics_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function collapsecommsanddemographics_civicrm_alterSettingsFolders(&$metaDataFolders = NULL)
-{
-    _collapsecommsanddemographics_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function collapsecommsanddemographics_civicrm_entityTypes(&$entityTypes)
-{
-    _collapsecommsanddemographics_civix_civicrm_entityTypes($entityTypes);
-}
-
-/**
- * Implements hook_civicrm_thems().
- */
-function collapsecommsanddemographics_civicrm_themes(&$themes)
-{
-    _collapsecommsanddemographics_civix_civicrm_themes($themes);
-}
-
 
 /**
  * Add icon field to Gender Option admin form
@@ -228,7 +105,6 @@ function collapsecommsanddemographics_civicrm_pageRun(&$page)
 
     }
 }
-
 
 /**
  * Implements hook_civicrm_contactSummaryBlocks().
